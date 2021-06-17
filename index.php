c8ec66264acdf016a60bd98866ec7d1ef7413507
unknown <jean-bateee@hotmail.fr>
2021-06-17 14:32:56 +0200

    add formulaire && cookies

--- index.php
+++ index.php
@@ -7,12 +7,6 @@ function isValid($pattern ,$subject){   //vérifie la regex puis renvoi vrai ou 
     }
 }
 
-function isSame($value1, $value2){ //compart si les mdp sont identique
-    if ($value1 == $value2) {
-        return true;
-    }
-}
-
 function mailExist($element, $array){ //compart le mail avec les mails existant et renvoi vrai si elle n'est pas trouvé
     if (in_array($element, $array)) {
         return false;
@@ -23,10 +17,14 @@ function mailExist($element, $array){ //compart le mail avec les mails existant 
 
 $name = htmlspecialchars($_POST['name'] ?? 'Vide');
 $firstname = htmlspecialchars($_POST['firstname'] ?? 'Vide') ;
+$age = htmlspecialchars($_POST['age'] ?? 'Vide') ;
+$gender = htmlspecialchars($_POST['gender'] ?? 'Vide') ;
+$zipCode = htmlspecialchars($_POST['zipCode'] ?? 'Vide') ;
 $email = htmlspecialchars($_POST['email'] ?? 'Vide') ;
-$password = $_POST['password'] ?? 'Vide' ;
-$confirm = $_POST['confirm'] ?? 'Vide' ;
+$searchGender = htmlspecialchars($_POST['searchGender'] ?? 'Vide') ;
 $regexName = "/^[a-z ,.'-]+$/i";
+$regexAge = "/^\d{1,2}$/";
+$regexZipCode = "/^\d{5}$/";
 $regexMail = "/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/";
 $regexPassword = "/^(?=.*?[A-Z])(?=.*?[a-z]).{5,}$/";
 $mailArray = ['julien@gmail.com', 'paul@gmail.com', 'habib@hotmail.fr'];
@@ -43,15 +41,15 @@ if (isset($_POST['submit'])) { //si submit est dans le post
         $count++;
         $classFirstname = 'is-invalid';
     }
-    if (!isSame($password, $confirm)) {
-        $errorPassword =  'Le mot de passe ne correspond pas';
+    if (!isValid($regexAge, $age)) {
+        $errorAge = 'Age incorrect, exemple : 25';
         $count++;
-        $classPassword = 'is-invalid';
+        $classAge = 'is-invalid';
     }
-    if (!isValid($regexPassword, $password)) {
-        $errorMdp = 'Mot de passe incorrect, exemple : 25KhjG';
+    if (!isValid($regexZipCode, $zipCode)) {
+        $errorZipCode = 'Code postal incorrect, exemple : 50310';
         $count++;
-        $classMdp = 'is-invalid';
+        $classZipCode = 'is-invalid';
     }
     if (!isValid($regexMail, $email)) {
         $errorMail = 'Mail incorrect, exemple : john@gmail.com';
@@ -63,10 +61,26 @@ if (isset($_POST['submit'])) { //si submit est dans le post
         $classMail = 'is-invalid';
     }
     if ($count == 0) { // le conteur est à 0
-        header("Location: welcome.php?name=$name&firstname=$firstname"); // change de page avec le bonne url pour récupéré en GET
-        exit(); // stop le script
+        header("Location: developpers.php?searchGender=$searchGender"); // change de page avec le bonne url pour récupéré en GET
     }
 }
+
+$pushToCookie = ['name', 'firstname', 'age', 'gender', 'zipCode', 'email', 'searchGender'];
+
+if (!empty($_POST)) {
+    foreach($pushToCookie as $key){
+        $value = $_POST[$key];
+        if (isset($value)) {
+            setcookie($key, $value, time() + 24 * 60 * 60);
+        }
+    }
+}
+
+if (isset($_COOKIE["name"])) {
+    header("Location: developpers.php?searchGender=$searchGender"); // change de page avec le bonne url pour récupéré en GET
+    exit(); // stop le script
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -80,37 +94,54 @@ if (isset($_POST['submit'])) { //si submit est dans le post
     <title>Formulaire</title>
 </head>
 <body>
-    <h1 class="mb-5">FORMULAIRE</h1>
+    <h1 class="mb-5">BIENVENUE</h1>
     <div class="myForm">
-        <form action="developpers.php" method="post" novalidate>
+        <form action="index.php" method="post" novalidate>
         <div class="mb-3">
             <label for="name" class="form-label">Nom</label>
-            <input type="text" class="form-control <?= $className ?? '' ?>" id="name" name="name" required value="<?= $name ?? '';?>" > <!-- si il ya le name dans POSt affiche le sinon met rien -->
+            <input type="text" class="form-control <?= $className ?? '' ?>" id="name" name="name" required value="<?= $_POST['name'] ?? '';?>" > <!-- si il ya le name dans POSt affiche le sinon met rien -->
             <div id="emailHelp" class="form-text"><?= $errorName ?? '' ?></div> <!-- affiche le message d'erreur -->
         </div>
         <div class="mb-3">
             <label for="firstname" class="form-label">Prénom</label>
-            <input type="text" class="form-control <?= $classFirstname ?? '' ?>" id="firstname" name="firstname" required value="<?= $firstname ?? '';?>">
+            <input type="text" class="form-control <?= $classFirstname ?? '' ?>" id="firstname" name="firstname" required value="<?= $_POST['firstname'] ?? '';?>">
             <div id="emailHelp" class="form-text"><?= $errorFirstname ?? '' ?></div>
         </div>
         <div class="mb-3">
+            <label for="age" class="form-label">Age</label>
+            <input type="number" class="form-control <?= $classAge ?? '' ?>" id="age" name="age" required value="<?= $_POST['age'] ?? '';?>">
+            <div id="emailHelp" class="form-text"><?= $errorAge ?? '' ?></div>
+        </div>
+        <div class="mb-3">
+            <label for="gender" class="form-label">Genre</label>
+            <select id="gender" class="form-select <?= $classGender ?? '' ?>" name="gender" required>
+                <option selected disabled>Veuillez choisir</option>
+                <option>Homme</option>
+                <option>Femme</option>
+            </select>
+            <div id="emailHelp" class="form-text"><?= $errorGender ?? '' ?></div>
+        </div>
+        <div class="mb-3">
+            <label for="zipCode" class="form-label">Code Postal</label>
+            <input type="text" class="form-control <?= $classZipCode ?? '' ?>" id="zipCode" name="zipCode" aria-describedby="emailHelp" required value="<?= $_POST['zipCode'] ?? '';?>">
+            <div id="emailHelp" class="form-text"><?= $errorZipCode ?? '' ?></div>
+        </div>
+        <div class="mb-3">
             <label for="email" class="form-label">Adresse mail</label>
-            <input type="email" class="form-control <?= $classMail ?? '' ?>" id="email" name="email" aria-describedby="emailHelp" required value="<?= $email ?? '';?>">
+            <input type="email" class="form-control <?= $classMail ?? '' ?>" id="email" name="email" aria-describedby="emailHelp" required value="<?= $_POST['email'] ?? '';?>">
             <div id="emailHelp" class="form-text"><?= $errorMail ?? '' ?></div>
         </div>
         <div class="mb-3">
-            <label for="password" class="form-label">Mot de passe</label>
-            <input type="password" class="form-control <?= $classMdp ?? '' ?>" id="password" name="password" required>
-            <div id="emailHelp" class="form-text"><?= $errorMdp ?? '' ?></div>
+            <label for="searchGender" class="form-label">Recherche</label>
+            <select id="searchGender" class="form-select <?= $classSearchGender ?? '' ?>" name="searchGender" required>
+                <option selected disabled>Veuillez choisir</option>
+                <option>Homme</option>
+                <option>Femme</option>
+            </select>
+            <div id="emailHelp" class="form-text"><?= $errorSearchGender ?? '' ?></div>
         </div>
-        <div class="mb-3">
-            <label for="confirm" class="form-label">Confirmation du mot de passe</label>
-            <input type="password" class="form-control <?= $classPassword ?? '' ?>" id="password2" name="confirm" required>
-            <div id="emailHelp" class="form-text"><?= $errorPassword ?? '' ?></div>
-        </div>
-        <button type="submit" class="btn btn-primary" id="btn" name="submit">S'INSCRIRE</button>
+        <button type="submit" class="btn btn-primary" id="btn" name="submit">Rencontrer nos célibataire</button>
         </form>
     </div>
-
 </body>
 </html>
